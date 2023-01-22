909a9371b08cf07b522c0cf0c9fe8c91115eb218
alexander gubernikoff <agubernikoff@gmail.com>
2023-01-22 17:42:48 -0500

    main feed subheaders

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Paralax from "./Paralax";
+import Section from "./Section";
 
 function Home() {
+  const sections = ["header 1", "header 2", "header 3", "header 4", "header 5"];
+  const mappedSections = sections.map((section) => (
+    <Section header={section} />
+  ));
   return (
     <div className="home">
       <h1>Thy Eschalot</h1>
       <Paralax />
+      <div className="section-container">{mappedSections}</div>
     </div>
   );
 }
